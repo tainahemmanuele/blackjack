406187de1bafbd56cbdcfb14dcd0cb2e4b31e163
Tainah Emmanuele <tainahemmanuele@gmail.com>
2019-08-03 22:04:18 -0300

    Feito até o modulo 6 do curso

---
+++ script.js
@@ -0,0 +1,39 @@
+// Blackjack
+// by Tainah Emmanuele, a partir do curso do PluralSight: 
+// JavaScript: Getting Started - by Mark Zamoyta
+
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+let values = ['Ace', 'King', 'Queen', 'Jack',
+    'Ten', 'Nine', 'Eight', 'Seven', 'Six',
+    'Five','Four', 'Three', 'Two', 'One'
+];
+
+
+function createDeck(){
+    let deck = [];
+    for (let suitIdx = 0; suitIdx < suits.length ; suitIdx++){
+      for (let valueIdx = 0 ; valueIdx < values.length ; valueIdx++){
+        deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+      }
+    }
+    return deck; 
+}
+
+let deck = createDeck();
+
+function getNextCard(){
+    return deck.shift();
+}
+
+for (let i=0; i < deck.length; i++){
+    console.log(deck[i]);
+  
+}
+
+let playerCards = [getNextCard(), getNextCard()];
+
+console.log("Welcome to Blackjack");
+
+console.log("You are dealt:");
+console.log(" "+ playerCards[0]);
+console.log(" "+ playerCards[1]);
